5988a5f3f4198378ce9523356e044ea271ba3c39
Eduardo Pinheiro <tm.twomaker@gmail.com>
2019-10-27 20:00:31 -0300

    Create nested route

--- src/App/View/Routes.tsx
+++ src/App/View/Routes.tsx
@@ -6,13 +6,16 @@ import Home from "./Scenes/Home";
 import Tests from "./Scenes/Tests";
 import Persons from "./Scenes/Persons";
 import TestGoingOn from "./Scenes/TestGoingOn";
+import Test from "./Scenes/Tests/ChildScene/Test";
 
 export default class Routes extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/testes" component={Tests} />
+        <Route path="/testes" component={Tests} >
+          <Route path="/testes/:testId" component={Test} />
+        </Route>
         <Route path="/pessoas" component={Persons} />
         <Route path="/testando" component={TestGoingOn} />
       </Switch>
