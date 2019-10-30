719f5e54ff646a990a2a070a9aa7c1393766c43d
Eduardo <feduardo191@gmail.com>
2019-10-29 23:36:57 -0300

    Fix title of test type

--- src/App/Controller/TestType/index.ts
+++ src/App/Controller/TestType/index.ts
@@ -23,17 +23,9 @@ export class TestType extends Controller {
 
     public static async getTestTypeById(id: number) {
         delay(1000);
-        const testType: TestTypeInterface = {
-            id: 1,
-            title: "Sou um titulo 1",
-            start_url: "https://www.youtube.com/watch?v=HVrmuR6xGc0",
-            objective: "Sou um objetivo",
-        }
-        /*
         const testType: TestTypeInterface = await Axios.get(`/test_type/${id}`).then((response) => {
             return response.data;
         });
-        */ 
         return testType;
     }
 
@@ -47,43 +39,9 @@ export class TestType extends Controller {
 
     public static async getTestTypes() {
         delay(500);
-        const testType: Array<TestTypeInterface> = [
-            {
-                id: 1,
-                title: "Sou um titulo asdasdasdasdasda 1",
-                start_url: "https://www.youtube.com/watch?v=HVrmuR6xGc0",
-                objective: "Sou um objetivo asdasdasdasdasd",
-            },
-            {
-                id: 2,
-                title: "Sou um titulo 2",
-                start_url: "https://www.youtube.com/watch?v=HVrmuR6xGc0",
-                objective: "Sou um objetivo",
-            },
-            {
-                id: 3,
-                title: "Sou um titulo 3",
-                start_url: "https://www.youtube.com/watch?v=HVrmuR6xGc0",
-                objective: "Sou um objetivo",
-            },
-            {
-                id: 4,
-                title: "Sou um titulo 4",
-                start_url: "https://www.youtube.com/watch?v=HVrmuR6xGc0",
-                objective: "Sou um objetivo",
-            },
-            {
-                id: 5,
-                title: "Sou um titulo 5",
-                start_url: "https://www.youtube.com/watch?v=HVrmuR6xGc0",
-                objective: "Sou um objetivo",
-            },
-        ]
-        /*
         const testType: Array<TestTypeInterface> = await Axios.get('/test_type').then((response) => {
             return response.data;
         });
-        */
         return testType;
     }
 }
