c590586fb42867c908c5868414764c20dd2d25cb
wiliamk <wiliam.keltke@gmail.com>
2020-07-14 18:03:53 -0300

    muda o text-align do manual e aplica os dados reais do manual

--- src/App/Data/Help.ts
+++ src/App/Data/Help.ts
@@ -1,26 +1,26 @@
 export const help = [
     {
         title: "Cadastrar novo usuário",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing."
+        description: 'Para cadastrar um novo usuário, clique no ícone "+", no canto superior direito da tela, e depois em Nova pessoa. Abrirá um modal com 3 campos a serem preenchidos. Basta clicar em cadastrar e o usuário será cadastrado com sucesso. Para cancelar a ação, clique em cancelar e retorne à tela anterior.'
     },
     {
         title: "Cadastrar novo teste",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing."
+        description: 'Para cadastrar um novo teste, no canto superior direito da tela, clique no ícone "+" e depois em Novo tipo de teste. Abrirá um modal com 3 campos a serem preenchidos. É importante observar que devemos inserir o protocolo na URL (http://). Basta clicar em cadastrar e o teste será cadastrado com sucesso. Para cancelar a ação, clique em cancelar e retorne à tela anterior.'
     },
     {
         title: "Iniciar novo teste",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing."
+        description: "Para iniciar um novo teste, no menu central, clique no ícone de um foguete no menu de navegação no canto inferior da tela. Selecione o Tipo de teste e a Pessoa. Clique em iniciar. O usuário será direcionado para a página configurada no momento da criação do Tipo do teste. Após a navegação, clique em Finalizar no canto inferior direito para que se encerre o teste."
     },
     {
-        title: "Consultar testes realizados",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing."
-    },
-    {
-        title: "Consultar usuários cadastrados",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing."
+        title: "Consultar usuários realizados",
+        description: "Para consultar os usuários cadastrados no sistema, clique no ícone de pessoas no menu de navegação no canto inferior da tela. Uma lista com todas as pessoas cadastradas será exibida. Ao clicar em algum usuário uma lista de todos os testes realizados por ele será exibida. Para obter mais informações de determinado teste, basta clicar no mesmo."
     },
     {
         title: "Consultar testes cadastrados",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing."
+        description: "Para consultar os testes cadastrados, clique no ícone de testes no menu de navegação no canto inferior da tela. Uma lista com os testes cadastrados no sistema será exibida. Se desejar consultar um teste individualmente, clique. Uma lista de todos os testes cadastrados no sistema selecionado será exibida. Ao clicar em um teste individualmente é possível verificar os gráficos resultantes"
+    },
+    {
+        title: "Consultar testes realizados",
+        description: "Na própria home o usuário pode consultar todos os testes realizados estratificados por usuários e tipo de teste, como mostrado na imagem. Ao clicar em um componente de teste, o resultado gráfico com as emoções será exibido, bem como os dados do participante, a URL do site e o título do teste."
     },
 ]
